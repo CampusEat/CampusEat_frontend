db4cf964a92c00336526b9944d710c5c56ad8598
mirae0831@o365.hufs.ac.kr <mirae0831@o365.hufs.ac.kr>
2023-08-18 16:43:00 +0900

    [chore] 모달창 크기, 닫기 버튼 변경

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,49 +1,24 @@
 import React from 'react';
-import "./modal.css"
-import styled from "styled-components";
+import "./modal.css";
 
+function Modal(props) {
 
-
-function Modal({ isOpen, closeModal, message }) {
-  return (
-    <ModalOverlay style={{ display: isOpen ? "flex" : "none" }}>
-      <ModalContent>
-        <button onClick={closeModal}>✕</button>
-        <p>{message}</p>
-      </ModalContent>
-    </ModalOverlay>
-  );
-}
-
-// Styled Components
-const ModalOverlay = styled.div`
-  position: fixed;
-  width: 100%;
-  height: 100%;
-  background-color: rgba(0, 0, 0, 0.7);
-  z-index: 100;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-
-const ModalContent = styled.div`
-  background-color: white;
-  padding: 20px;
-  padding-top:0%;
-  border-radius: 5px;
-
-  button {
-    font-size: 20px;
-    color: #4C4A4B;
-    border-radius: 15px;
-    height: 5px;
-    width: 10px;
-    background-color:#ffffff;
-    box-shadow: none;
+  function closeModal(){
+    props.closeModal();
   }
-    
+
+  return (
+    <div className='ModalBack'>
+      <div className="ModalContent">
+      <button className='Closebtn' onClick={closeModal}>
+          ✖
+        </button>
+        {props.children}
+      </div>
+    </div>
+  );
+
 }
-`;
+
 
 export default Modal;
